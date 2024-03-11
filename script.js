ff32e98ee54b142f421bac1403e88f0c6891aff5
Alvin Gille <alvin.gille@elev.ga.ntig.se>
2024-03-11 11:03:37 +0100

    donso

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 let rubrik = document.querySelector("h1");
-rubrik.textContent="potate";
+let sidfot = document.querySelector("footer");
+let men = document.querySelector("main");
+let navi = document.querySelector("nav");
+let prod = document.querySelector(".produkt");
+let bod = document.querySelector("body");
+let sö = document.querySelector(".Söt");
+let mjö = document.querySelector(".Mjölig");
+let sm = document.querySelector(".Små");
+let am = document.querySelector(".Amandine");
+let ma = document.querySelector(".Mandel");
+
 
 function change_mode()
 {
     console.log("CHNAGE")
     
     rubrik.classList.toggle("dark");
+
+    sidfot.classList.toggle("dark");
+
+    men.classList.toggle("dark");
+
+    navi.classList.toggle("dark");
+
+    prod.classList.toggle("dark");
+
+    bod.classList.toggle("dark");
+
+    sö.classList.toggle("dark");
+    mjö.classList.toggle("dark");
+    sm.classList.toggle("dark");
+    am.classList.toggle("dark");
+    ma.classList.toggle("dark");
+
 }
